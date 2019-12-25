577a4a59829361e927204b5d19d4b9f91d9d753e
rabiulhasan3 <rabiul.fci@gmail.com>
2019-12-25 20:51:13 +0600

    theme setup/bootstraping

--- wp-content/themes/philosophy/functions.php
+++ wp-content/themes/philosophy/functions.php
@@ -5,7 +5,6 @@ function philosophy_setup_theme(){
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'post-formats', array( 'aside', 'gallery', 'audio', 'video', 'standard', 'quote' ) );
     add_theme_support( 'html5', array( 'comment-form', 'search-form' ) );
-    add_editor_style( 'editor-style.css' )
-
+    add_editor_style( 'editor-style.css' );
 }
 add_action("after_setup_theme","philosophy_setup_theme");
